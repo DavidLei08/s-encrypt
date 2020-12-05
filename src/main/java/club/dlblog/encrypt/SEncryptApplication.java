570fa0a83d3839e4976830a0af6cd3866d45c3aa
DavidLei08 <2686849744@qq.com>
2020-12-05 16:19:49 +0800

    常用加解密方法

---
+++ src/main/java/club/dlblog/encrypt/SEncryptApplication.java
@@ -0,0 +1,13 @@
+package club.dlblog.encrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SEncryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SEncryptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/dlblog/encrypt/SEncryptApplicationTests.java
@@ -0,0 +1,13 @@
+package club.dlblog.encrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SEncryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
